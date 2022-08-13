2b3bbced8629b29c88622ba6b97bb47f530018eb
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2022-08-13 08:48:23 -0700

    Create UUID_RAII.cpp

---
+++ UUID_RAII.cpp
@@ -0,0 +1,39 @@
+#include <Rpc.h>
+#pragma comment(lib, "Rpcrt4.lib")
+
+class UUID_RAII
+{
+#ifdef UNICODE
+	typedef RPC_WSTR RPC_STR;
+#else
+	typedef RPC_CSTR RPC_STR;
+#endif // UNICODE
+
+public:
+	UUID_RAII() {
+		if (UuidCreate(&mUUID) == RPC_S_UUID_NO_ADDRESS) {
+			return;
+		}
+
+		mStatus = UuidToString(&mUUID, &mUUIDString);
+	}
+
+	~UUID_RAII() {
+		if (!mStatus) {
+			RpcStringFree(&mUUIDString);
+		}
+	}
+
+	inline const auto& GetUUIDString() const {
+		return mUUIDString;
+	}
+
+	inline const UUID& GetUUID() const {
+		return mUUID;
+	}
+
+private:
+	::UUID mUUID{};
+	RPC_STR mUUIDString = nullptr;
+	RPC_STATUS mStatus = 0;
+};
