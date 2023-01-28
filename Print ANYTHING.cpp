6324b1b2d16a686aaa962413d9418b25907e31d7
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2023-01-28 19:33:42 +0200

    Update Print ANYTHING.cpp

--- Print ANYTHING.cpp
+++ Print ANYTHING.cpp
@@ -7,7 +7,6 @@ using namespace std;
 template<typename Object, typename Iterable>
 void Print(
     const Iterable& iterable,
-    const string& separatorDimension = "\n",
     const function<void(const Object&)>& funcPrintElem = [] (const auto& obj) {
         static_assert(is_arithmetic_v<Object>, R"(The object from the innermost range is not a built-in type, please provide a valid function.)");
         cout << obj << " ";
@@ -15,7 +14,7 @@ void Print(
 ) {
     if constexpr (ranges::range<Iterable>) {
         ranges::for_each(iterable, [&] (const auto& it) { Print(it, funcPrintElem); });
-        cout << separatorDimension;
+        cout << endl;
     } else {
         funcPrintElem(iterable);
     }
