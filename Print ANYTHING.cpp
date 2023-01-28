1c19cd5b6d82372aa7084ee34dbcd5611296383f
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2023-01-28 22:08:37 +0200

    Update Print ANYTHING.cpp

--- Print ANYTHING.cpp
+++ Print ANYTHING.cpp
@@ -4,17 +4,19 @@
 
 using namespace std;
 
-template<typename Object, typename Iterable>
+template<typename Object, typename Iterable, typename Stream = ostream>
 void Print(
     const Iterable& iterable,
-    const function<void(const Object&)>& funcPrintElem = [] (const auto& obj) {
+    const string& separator = "\n",
+    const function<void(const Object&)>& funcPrintElem = [] (const Object& obj) {
         static_assert(is_arithmetic_v<Object>, R"(The object from the innermost range is not a built-in type, please provide a valid function.)");
         cout << obj << " ";
-    }
+    },
+    const Stream& stream = cout
 ) {
     if constexpr (ranges::range<Iterable>) {
-        ranges::for_each(iterable, [&] (const auto& it) { Print(it, funcPrintElem); });
-        cout << endl;
+        ranges::for_each(iterable, [&] (const auto& it) { Print(it, separator, funcPrintElem, stream); });
+        cout << separator;
     } else {
         funcPrintElem(iterable);
     }
