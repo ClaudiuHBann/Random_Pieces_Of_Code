6aa4758ed3e24428d46e07136a226753522dfdc4
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2023-03-23 23:11:44 +0200

    Update Global Accelerator.cpp

--- Global Accelerator.cpp
+++ Global Accelerator.cpp
@@ -5,29 +5,36 @@
 
 using namespace std;
 
-class Accelerator {
-public:
-	inline void Register(const vector<uint8_t>& keys, const function<void()>& callback) {
-		mAccelerators.push_back({ keys, callback, false });
-	}
+class Accelerator
+{
+  public:
+    inline void Register(const vector<uint8_t> &keys, const function<void()> &callback)
+    {
+        mAccelerators.push_back({keys, callback, false});
+    }
 
-	void Check() {
-		for (auto& accelerator : mAccelerators) {
-			bool fire = true;
-			for (const auto& key : get<0>(accelerator)) {
-				if (GetAsyncKeyState(key) >= 0) {
-					fire = false;
-					break;
-				}
-			}
+    void Check()
+    {
+        for (auto &[keys, callback, state] : mAccelerators)
+        {
+            bool fire = true;
+            for (const auto &key : keys)
+            {
+                if (GetAsyncKeyState(key) >= 0)
+                {
+                    fire = false;
+                    break;
+                }
+            }
 
-			if (!get<2>(accelerator) && fire) {
-				get<1>(accelerator)();
-			}
-			get<2>(accelerator) = fire;
-		}
-	}
+            if (!state && fire)
+            {
+                callback();
+            }
+            state = fire;
+        }
+    }
 
-private:
-	vector<tuple<vector<uint8_t>, function<void()>, bool>> mAccelerators {};
+  private:
+    vector<tuple<vector<uint8_t>, function<void()>, bool>> mAccelerators{};
 };
