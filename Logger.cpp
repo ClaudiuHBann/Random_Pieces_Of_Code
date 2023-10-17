41a257499e8f4e8630ebfabac25f90ed360283fb
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2023-10-17 04:09:03 +0300

    Update Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -1,132 +1,123 @@
+// win32
 #include <Windows.h>
 
+// std
 #include <functional>
-#include <algorithm>
 #include <iostream>
 #include <sstream>
-#include <string>
-#include <ranges>
 
-namespace Utility {
-	using namespace std;
+#define TRACE_LOCATION_PROCESS_THREAD_ID                                                                               \
+    hbann::String(TEXT("PID: ") + hbann::ToString(::GetCurrentProcessId()) + TEXT(" TID: ") +                          \
+                  hbann::ToString(::GetCurrentThreadId()))
+#define TRACE_LOCATION_FILE_LINE hbann::ClampFileNameLength(TEXT(__FILE__), hbann::ToString(__LINE__), 50)
+#define TRACE_LOCATION                                                                                                 \
+    hbann::String(TRACE_LOCATION_PROCESS_THREAD_ID + TEXT(" ") + TRACE_LOCATION_FILE_LINE + TEXT("\t"))
+#define TRACE(strn) hbann::Print<const ::TCHAR *>((TRACE_LOCATION + (hbann::StringStream() << strn).str()).c_str())
 
-	namespace impl {
-		template<typename T>
-		constexpr bool is_basic_string_v = false;
+namespace hbann
+{
+namespace detail
+{
+template <typename Type> constexpr bool is_basic_string_v = false;
 
-		template<typename... T>
-		constexpr bool is_basic_string_v<basic_string<T...>> = true;
-	}
+template <typename... Type> constexpr bool is_basic_string_v<std::basic_string<Type...>> = true;
+} // namespace detail
 
-	template<typename T>
-	constexpr bool is_basic_string_v = impl::is_basic_string_v<T>;
+template <typename Type> constexpr bool is_basic_string_v = hbann::detail::is_basic_string_v<Type>;
+
+template <typename Type> using remove_decayed_cvptr_t = std::remove_const_t<std::remove_pointer_t<std::decay_t<Type>>>;
 
 #if defined(_UNICODE) || defined(UNICODE)
-	using String = wstring;
-	using StringStream = wstringstream;
+using String = std::wstring;
+using StringStream = std::wstringstream;
 #else
-	using String = string;
-	using StringStream = stringstream;
+using String = std::string;
+using StringStream = std::stringstream;
 #endif // defined(_UNICODE) || defined(UNICODE)
 
-	template <typename T>
-	static inline String ToString(const T& t) {
-		if constexpr (is_same_v<remove_const_t<remove_pointer_t<decay_t<T>>>, ::TCHAR> ||
-					  is_same_v<remove_const_t<remove_reference_t<decay_t<T>>>, String>) {
-			return t;
-		} else {
+template <typename String> [[nodiscard]] static constexpr hbann::String ToString(const String &aString) noexcept
+{
+    if constexpr (std::is_same_v<hbann::remove_decayed_cvptr_t<String>, ::TCHAR> ||
+                  is_basic_string_v<hbann::remove_decayed_cvptr_t<String>>)
+    {
+        return aString;
+    }
+    else
+    {
 #if defined(_UNICODE) || defined(UNICODE)
-			return to_wstring(t);
+        return std::to_wstring(aString);
 #else
-			return to_string(t);
+        return std::to_string(aString);
 #endif // defined(_UNICODE) || defined(UNICODE)
-		}
-	}
-
-	template<typename StringType, typename StringFrom>
-	static inline auto ToStringType(const StringFrom& strTo) {
-		static_assert(is_basic_string_v<StringFrom>, "The StringFrom is not a basic_string!");
-		static_assert(is_arithmetic_v<StringType>, "The StringType is not a built-in type!");
-
-		if constexpr (is_same_v<typename StringFrom::value_type, StringType>) {
-			return strTo;
-		} else {
-#pragma warning(suppress: 4244)
-			return basic_string<StringType, char_traits<StringType>, allocator<StringType>>(strTo.begin(), strTo.end());
-		}
-	}
-
-	static inline String ClampFileNameLength(
-		String file,
-		String line,
-		const size_t lengthMaxFileName = 69,
-		const size_t lengthMaxLine = 4,
-		const String& padding = TEXT("...")
-	) {
-		line += String(lengthMaxLine - line.length(), TEXT(' '));
-		String lineStr(TEXT(":") + line);
-
-		auto fileLength = file.length();
-		if (fileLength <= lengthMaxFileName) {
-			file += lineStr;
-			file += String(lengthMaxFileName - fileLength, TEXT(' '));
-		} else {
-			auto start = fileLength - lengthMaxFileName + padding.length();
-			file = padding + file.substr(start);
-			file += lineStr;
-		}
-
-		return file;
-	}
-
-	static inline void OutputDebugStringForced(const ::TCHAR* str) {
-		if (::IsDebuggerPresent()) {
-			::OutputDebugString(str);
-		}
-
-		if (::GetConsoleWindow()) {
-#if defined(_UNICODE) || defined(UNICODE)
-			wcout << str;
-#else
-			cout << str;
-#endif // defined(_UNICODE) || defined(UNICODE)
-		}
-
-		// TO DO: add a log file
-	}
-
-	template<typename Object, typename Iterable>
-	static void Print(
-		const Iterable& iterable,
-		const String& separatorDimensions = TEXT("\n"),
-		const function<void(const Object&)>& funcPrintElem = [] (const auto& obj) {
-			if constexpr (is_arithmetic_v<decay_t<Object>>) {
-				OutputDebugStringForced(ToString(obj).c_str());
-			} else if constexpr (is_same_v<remove_const_t<remove_reference_t<decay_t<Object>>>, String>) {
-				OutputDebugStringForced(obj.c_str());
-			} else if constexpr (is_same_v<remove_const_t<remove_pointer_t<decay_t<Object>>>, ::TCHAR>) {
-				OutputDebugStringForced(obj);
-			} else {
-				static_assert(false, R"(The object from the innermost range is not a built-in/(c)string type, please provide a valid print element function.)");
-			}
-
-			OutputDebugStringForced(TEXT(" "));
-		}
-	) {
-		if constexpr (ranges::range<Iterable>) {
-			OutputDebugStringForced(separatorDimensions.c_str());
-			ranges::for_each(iterable, [&] (const auto& it) { Print(it, separatorDimensions, funcPrintElem); });
-		} else {
-			funcPrintElem(iterable);
-			OutputDebugStringForced(separatorDimensions.c_str());
-		}
-	}
+    }
 }
 
-#define TRACE_LOCATION_PROCESS_THREAD_ID Utility::String(TEXT("PID: ") + Utility::ToString(::GetCurrentProcessId()) + TEXT(" TID: ") + Utility::ToString(::GetCurrentThreadId()))
-#define TRACE_LOCATION_FILE_LINE Utility::ClampFileNameLength(TEXT(__FILE__), Utility::ToString(__LINE__), 50)
-#define TRACE_LOCATION Utility::String(TRACE_LOCATION_PROCESS_THREAD_ID + TEXT(" ") + TRACE_LOCATION_FILE_LINE + TEXT("\t"))
-#define TRACE(strn) Utility::Print<const ::TCHAR*>((TRACE_LOCATION + (Utility::StringStream() << strn).str()).c_str())
+[[nodiscard]] static inline auto ClampFileNameLength(hbann::String aFile, hbann::String aLine,
+                                                     const size_t aLengthMaxFileName = 69,
+                                                     const size_t aLengthMaxLine = 4,
+                                                     const hbann::String &aPadding = TEXT("..."))
+{
+    aLine += hbann::String(aLengthMaxLine - aLine.length(), TEXT(' '));
+    hbann::String lineStr(TEXT(":") + aLine);
+
+    auto fileLength = aFile.length();
+    if (fileLength <= aLengthMaxFileName)
+    {
+        aFile += lineStr;
+        aFile += hbann::String(aLengthMaxFileName - fileLength, TEXT(' '));
+    }
+    else
+    {
+        auto start = fileLength - aLengthMaxFileName + aPadding.length();
+        aFile = aPadding + aFile.substr(start);
+        aFile += lineStr;
+    }
+
+    return aFile;
+}
+
+static inline void OutputDebugStringForced(const ::TCHAR *aString) noexcept
+{
+    if (::IsDebuggerPresent())
+    {
+        ::OutputDebugString(aString);
+    }
+
+    if (::GetConsoleWindow())
+    {
+#if defined(_UNICODE) || defined(UNICODE)
+        std::wcout << aString;
+#else
+        std::cout << aString;
+#endif // defined(_UNICODE) || defined(UNICODE)
+    }
+
+    // TO DO: add a log file
+}
+
+template <typename Object, typename Range>
+static constexpr void Print(
+    const Range &aRange, const hbann::String &aSeparatorDimensions = TEXT("\n"),
+    const std::function<void(const Object &)> &aFuncPrintElem = [](const auto &aObject) {
+        OutputDebugStringForced(hbann::ToString(aObject).c_str());
+        OutputDebugStringForced(TEXT(" "));
+    }) noexcept
+{
+    if constexpr (std::ranges::range<Range>)
+    {
+        OutputDebugStringForced(aSeparatorDimensions.c_str());
+        for (const auto &object : aRange)
+        {
+            Print(object, aSeparatorDimensions, aFuncPrintElem);
+        }
+    }
+    else
+    {
+        aFuncPrintElem(aRange);
+        OutputDebugStringForced(aSeparatorDimensions.c_str());
+    }
+}
+} // namespace hbann
 
 int main() {
 	TRACE("wow" << TEXT(" this ") << 44);
