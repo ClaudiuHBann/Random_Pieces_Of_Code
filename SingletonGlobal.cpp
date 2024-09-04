36633cfd2ba94506c64353124d1eab3015062447
Hermann Claudiu-Andrei <123uidualcnnamreh@gmail.com>
2024-09-04 23:46:30 +0300

    Create SingletonGlobal.cpp

---
+++ SingletonGlobal.cpp
@@ -0,0 +1,209 @@
+#include <Windows.h>
+
+#include <cassert>
+#include <functional>
+#include <mutex>
+
+class UnMoveable
+{
+  protected:
+    constexpr UnMoveable() noexcept = default;
+    constexpr virtual ~UnMoveable() noexcept = default;
+
+  private:
+    constexpr UnMoveable(UnMoveable &&) noexcept = delete;
+    constexpr UnMoveable &operator=(UnMoveable &&) noexcept = delete;
+};
+
+class UnCopyable
+{
+  protected:
+    constexpr UnCopyable() noexcept = default;
+    constexpr virtual ~UnCopyable() noexcept = default;
+
+  private:
+    constexpr UnCopyable(const UnCopyable &) noexcept = delete;
+    constexpr UnCopyable &operator=(const UnCopyable &) noexcept = delete;
+};
+
+template <typename Type> class ContainerLazyPtrRaw final : public UnCopyable, public UnMoveable
+{
+    static_assert(!std::is_pointer_v<Type>, "The Type cannot be a raw pointer itself!");
+
+  public:
+    constexpr ContainerLazyPtrRaw() noexcept = default;
+
+    ~ContainerLazyPtrRaw()
+    {
+        if (mCallbackUninitialize)
+            mCallbackUninitialize();
+    }
+
+    void SetUninitialize(std::move_only_function<void()> aCallback) noexcept
+    {
+        mCallbackUninitialize = std::move(aCallback);
+    }
+
+    void SetPtr(Type *aPtr) noexcept
+    {
+        mPtr = aPtr;
+    }
+
+    Type *GetPtr() const noexcept
+    {
+        return mPtr;
+    }
+
+  private:
+    Type *mPtr{};
+
+    std::move_only_function<void()> mCallbackUninitialize;
+};
+
+class SharedMemory final
+{
+  public:
+    constexpr SharedMemory() noexcept = default;
+    constexpr ~SharedMemory() noexcept = default;
+
+    // If the name matches an existing mapping object, the function returns that mapped object
+    void *Create(const uint32_t aSize, std::string_view aName)
+    {
+        if (mHandle)
+            return mMemory;
+
+        mHandle = CreateFileMappingA(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, aSize, aName.data());
+        if (!mHandle)
+            throw std::runtime_error("mHandle cannot be null!");
+
+        mMemory = MapViewOfFile(mHandle, SECTION_MAP_WRITE | SECTION_MAP_READ, 0, 0, 0);
+        if (!mMemory)
+            throw std::runtime_error("mMemory cannot be null!");
+
+        return mMemory;
+    }
+
+    template <typename Type> Type GetMemory(const size_t aOffset = 0) const noexcept
+    {
+        assert(mMemory);
+        uint8_t *memoryAsBytes = reinterpret_cast<uint8_t *>(mMemory);
+        return reinterpret_cast<Type>(memoryAsBytes + aOffset);
+    }
+
+    void Delete() noexcept
+    {
+        if (mMemory)
+            UnmapViewOfFile(mMemory);
+
+        if (mHandle)
+            CloseHandle(mHandle);
+    }
+
+  private:
+    void *mMemory{};
+    void *mHandle{};
+};
+
+// A Per Process Singleton that uses shared memory
+//
+// The shared memory layout is:
+//  2 bytes  +  the size of the object
+// ref count      the object itself
+//
+// The File Mapping has the name format: SingletonGlobal<typeid(Type).hash_code()>
+template <typename Type> class SingletonGlobal : public UnCopyable, public UnMoveable
+{
+    using RefCountType = uint16_t;
+
+  public:
+    constexpr SingletonGlobal() noexcept = default;
+
+    virtual ~SingletonGlobal() = default;
+
+    static Type &GetInstance()
+    {
+        return CreateInstance();
+    }
+
+  private:
+    static inline ContainerLazyPtrRaw<Type> mInstance;
+    static inline std::mutex mMutex;
+
+    static inline SharedMemory mSharedMemory;
+
+    static RefCountType &GetCount() noexcept
+    {
+        return *mSharedMemory.GetMemory<RefCountType *>();
+    }
+
+    static Type *GetObjectPtr() noexcept
+    {
+        return mSharedMemory.GetMemory<Type *>(sizeof(RefCountType));
+    }
+
+    static Type &CreateInstance()
+    {
+        std::lock_guard<decltype(mMutex)> lock(mMutex);
+
+        if (mInstance.GetPtr())
+            return *mInstance.GetPtr();
+
+        return Initialize();
+    }
+
+    static void DeleteInstance()
+    {
+        std::lock_guard<decltype(mMutex)> lock(mMutex);
+
+        if (!mInstance.GetPtr())
+            return;
+
+        Uninitialize();
+    }
+
+    static Type &Initialize()
+    {
+        // contains the instance count and the object itself
+        const uint32_t sharedMemorySize = sizeof(RefCountType) + sizeof(Type);
+        const auto sharedMemoryName = std::format("SingletonGlobal<{}>", typeid(Type).hash_code());
+
+        mSharedMemory.Create(sharedMemorySize, sharedMemoryName.c_str());
+
+        if (GetCount()++)
+        {
+            // the object was already created
+            mInstance.SetPtr(GetObjectPtr());
+        }
+        else
+        {
+            // use the placement new operator to create our object
+            // inside the shared memory we allocated
+            mInstance.SetPtr(new (GetObjectPtr()) Type());
+        }
+
+        // when this instance of the class will deallocate
+        // it will call this callback to delete the instance
+        mInstance.SetUninitialize(DeleteInstance);
+
+        return *mInstance.GetPtr();
+    }
+
+    static void Uninitialize()
+    {
+        if (--GetCount())
+            return;
+
+        // we cannot call the delete operator on this object
+        // in conjunction with the placement new operator
+        // therefore we call it's dtor explicitly
+        mInstance.GetPtr()->~Type();
+
+        mSharedMemory.Delete();
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
